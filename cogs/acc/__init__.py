7b5d2dd32c1d3a1a729f7a07aacf4e627395f638
sabi15 <thewolf7015@gmail.com>
2026-04-25 17:54:32 +0530

    Update __init__.py

--- cogs/acc/__init__.py
+++ cogs/acc/__init__.py
@@ -1,15 +1,27 @@
+import discord
 from discord import app_commands
-from discord.ext import commands
-from .add import add
-from .remove import remove
-from .history import history
-from .warweight import warweight
+from discord.ext import commands as ext_commands
+from .commands.add import add
+from .commands.remove import remove
+from .commands.view import view
+from .commands.history import history
 
-acc_group = app_commands.Group(name="acc", description="Account commands")
-acc_group.command(name="add")(add)
-acc_group.command(name="remove")(remove)
-acc_group.command(name="history")(history)
-acc_group.command(name="warweight")(warweight)
+class acc(ext_commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.acc = app_commands.Group(
+            name="acc",
+            description="public commands"
+        )
+        self.acc.command(name="add", description="Add a permission to a role")(add)
+        self.acc.command(name="view", description="Remove a permission from a role")(view)
+        self.acc.command(name="history", description="Clear all perms in this server")(history)
+        self.acc.command(name="remove", description="List all perm assignments in this server")(remove)
+        bot.tree.add_command(self.acc)
+
+    async def cog_unload(self):
+        self.bot.tree.remove_command("acc")
+
 
 async def setup(bot):
-    bot.tree.add_command(acc_group)
+    await bot.add_cog(acc(bot))
