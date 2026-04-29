cf86855e4955e49d1a09c1eabafca043897477de
sabi15 <thewolf7015@gmail.com>
2026-04-30 04:59:46 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -14,13 +14,6 @@ async def create_tables():
     with open(schema_path, "r") as f:
         schema = f.read()
     async with _pool.acquire() as conn:
-        # Temporarily drop old tables to fix schema mismatch
-        await conn.execute("""
-            DROP TABLE IF EXISTS acc_accounts CASCADE;
-            DROP TABLE IF EXISTS acc_history CASCADE;
-            DROP TABLE IF EXISTS acc_usage CASCADE;
-        """)
-        # Recreate all tables from schema
         await conn.execute(schema)
 
 async def get_pool():
